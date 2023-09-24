0310a84770cf5c820561ea721a82ac75476b2b90
AdamClark <20ahclark@gmail.com>
2023-09-24 20:16:17 +0000

    This version of the git is git commit one not sure if it shouldve been made yet but here it is now and hoping it works hopefully can come back to this in a pinch

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `my csc496`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+
+  ],
+}
