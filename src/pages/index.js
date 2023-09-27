e571239fee0519407be6ef22894c7daded20e6d5
AdamClark <20ahclark@gmail.com>
2023-09-27 22:25:51 +0000

    Finished gatsby tutorial part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <MyImage></MyImage>
+      <MyImage>Text in the image tag</MyImage>
     </Layout>
   )
 }
