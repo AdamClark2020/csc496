bf4507238a29f317be09a74670891bd31b05f533
AdamClark <20ahclark@gmail.com>
2023-09-27 22:53:36 +0000

    Finished gatsby tutorial part 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,19 @@ import Layout from '../components/layout'
 import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import MyImage from "../components/myimage"
+import Seo from '../components/seo'
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <MyImage>Text in the image tag</MyImage>
+      <MyImage>Text in image</MyImage>
     </Layout>
   )
 }
 
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Home Page" />
 
 // Step 3: Export your component
 export default IndexPage
