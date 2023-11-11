67720198f1a9aef3ea911d3819d66ab85f243c4c
AdamClark <20ahclark@gmail.com>
2023-11-11 01:04:28 +0000

    Article pages rendering properly

--- src/components/myimage.js
+++ src/components/myimage.js
@@ -3,9 +3,16 @@ import { Link } from "gatsby"
 import { StaticImage } from 'gatsby-plugin-image'
 const MyImage = (props) =>{
     return(
+      
         <>
-        hi
+        <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />{props.children}
+
+      
     </>
+    
     )
 
 }
--- src/pages/article.js
+++ src/pages/article.js
@@ -1,20 +1,58 @@
-import {graphql} from 'gatsby'
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-const pageTemplate = props => {
-    const data = {
-        nodeArticle: props.pageContext.data
-    }
-    console.log(data);
-    console.log(props);
+const CenteredTitle = styled.h1`
+  text-align: center;
+`;
 
-    return(
-        <>
-        <h4>{data.nodeArticle?.title}</h4>
-        <p>{data.nodeArticle?.author.displayName}</p>
-        <p>{data.nodeArticle?.body.processed}</p>
-        <p>{data.nodeArticle?.mediaImage.url}</p>
+const Author = styled.p`
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.2em;
+  font-family: 'Minimalist Font', sans-serif;
+`;
 
-        </>
-    )
-}
+const CenteredContainer = styled.div`
+  text-align: center;
+  padding: 0 15%;
+  background: #f5f0ff; 
+  max-width: 70%; /* Limiting the width of the container */
+  margin: auto; /* Centering the container horizontally */
+
+  h2,
+  h3,
+  h4,
+  h5 {
+    font-family: 'Slim Font', cursive;
+    font-weight: 300;
+  }
+`;
+
+const Article = (props) => {
+  const data = props.pageContext.data;
+  if (!data || !data.title || !data.body) {
+    console.error('Article data is not available:', data);
+    return <p>Article data is not available</p>;
+  }
+
+  return (
+    <div style={{ background: '#d5f1e3' }}> {}
+      <CenteredTitle>{data.title}</CenteredTitle>
+      <Author>Author: {data.author?.displayName}</Author>
+      <CenteredContainer>
+        <div dangerouslySetInnerHTML={{ __html: data.body.processed }} />
+      </CenteredContainer>
+      <img
+        src={data.mediaImage?.mediaImage?.url}
+        alt={data.title}
+        style={{
+          display: 'block',
+          margin: 'auto',
+          border: '20px solid #8d5524', 
+        }}
+      />
+    </div>
+  );
+};
+
+export default Article;
